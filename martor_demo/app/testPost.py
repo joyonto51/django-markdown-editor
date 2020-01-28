ebda4fe66ea123423dd068226f463eb13db51701
Jayanta Roy <joyonto51@gmail.com>
2020-01-28 23:18:17 +0600

    admin and test file updated

--- martor_demo/app/testPost.py
+++ martor_demo/app/testPost.py
@@ -3,6 +3,7 @@ from app.models import Post
 
 
 class PostTestCase(TestCase):
+
     def testPost(self):
         post = Post(title="My Title", description="Blurb", wiki="Post Body")
         self.assertEqual(post.title, "My Title")
